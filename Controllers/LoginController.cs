c8796d01a9c32b37693e6a1a7530c9a44e076433
KhushbooKhushi <khushboobscity8271@gmail.com>
2019-04-23 16:36:40 +0530

    Add files via upload

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineRegistration.Models;
+
+namespace OnlineRegistration.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            StringUtils.ToCamelCase("k hs ushk");
+            return View();
+        }
+
+        public ActionResult NewRegistration()
+        {
+            return View();
+        }
+
+        public ActionResult UploadDocument()
+        {
+            return View();
+        }
+
+
+        public JsonResult RegistrationCompleted(Student Student)
+        {
+            try
+            {
+                OnlineRegistrationDataContext dataContext = new OnlineRegistrationDataContext();
+
+                //var list = (from d in dataContext.AdminLogins
+                //            where d.AdminName.ToUpper().Trim() == AdminLogin.AdminName.ToUpper().Trim() && d.AdminPassword == AdminLogin.AdminPassword.Trim()
+                //            select d);
+                //if (list.Any())
+                return Json("Success", JsonRequestBehavior.AllowGet);
+                //else
+                //    return Json("Not-Success", JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                return Json("Fail", JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        public JsonResult LoginResult(AdminLogin AdminLogin)
+        {
+            try
+            {
+                OnlineRegistrationDataContext dataContext = new OnlineRegistrationDataContext();
+
+                var list = (from d in dataContext.AdminLogins
+                            where d.AdminName.ToUpper().Trim() == AdminLogin.AdminName.ToUpper().Trim() && d.AdminPassword == AdminLogin.AdminPassword.Trim()
+                            select d);
+                if (list.Any())
+                    return Json("Success", JsonRequestBehavior.AllowGet);
+                else
+                    return Json("Not-Success", JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                return Json("Fail", JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ Controllers/UploadDocumentController.cs
@@ -0,0 +1,67 @@
+﻿using OnlineRegistration.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineRegistration.Controllers
+{
+    public class UploadDocumentController : Controller
+    {
+        //
+        // GET: /UploadDocument/
+
+        public ActionResult UploadDocumentView()
+        {
+            return View();
+        }
+
+
+        public JsonResult SaveDocument(Document doucment)
+        {
+            try
+            {
+                OnlineRegistrationDataContext dbContext = new OnlineRegistrationDataContext();
+                var jsonData = "";
+                if (doucment != null)
+                {
+                    Document doc = new Document();
+                    //attraction.AttractionId = model.AttractionId;
+                    //attraction.CityId = model.CityId;
+                    //attraction.AttractionTitle = model.AttractionTitle;
+                    //attraction.AttractionDetails = model.AttractionDetails;
+
+                    //at.PhotoPath = li.
+                    //if (!string.IsNullOrWhiteSpace(model.AttractionPhoto.ToString()))
+                    //{
+                    //    string pic = Guid.NewGuid().ToString() + ".jpg";
+                    //    byte[] imageBytes = Convert.FromBase64String(model.AttractionPhoto.ToString());
+
+                    //    MemoryStream ms = new MemoryStream(imageBytes, 0, imageBytes.Length);
+                    //    ms.Write(imageBytes, 0, imageBytes.Length);
+                    //    System.Drawing.Image image = System.Drawing.Image.FromStream(ms, true);
+
+                    //    image.Save(Server.MapPath("~/images/" + pic));
+
+                    //    attraction.AttractionPhoto = pic;
+                    //}
+
+                    //dbContext.Attractions.Add(attraction);
+                    //dbContext.SaveChanges();
+                    //jsonData = "Information Saved Successfully !!";
+                }
+                else
+                {
+                    jsonData = "Information Not Found !!";
+                }
+                return Json(jsonData, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { errorMsg = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
