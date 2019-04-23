838e500545dd66b5579e4643d86e0c1d9885c1fc
KhushbooKhushi <khushboobscity8271@gmail.com>
2019-04-23 16:39:36 +0530

    Add files via upload

---
+++ Angular/Service.js
@@ -0,0 +1,52 @@
+﻿/// <reference path="Module.js" />
+
+
+app.service("LoginService", function ($http) {
+    this.LoginResult = function (AdminLogin) {
+
+        var response = $http({
+            method: "post",
+            url: "Login/LoginResult",
+            data: JSON.stringify(AdminLogin),
+            dataType: "json"
+        });
+        return response;
+    }
+
+    this.RegistrationCompleted = function (Student) {
+        alert(Student);
+        var response = $http({
+            method: "post",
+            url: "Login/RegistrationCompleted",
+            data: JSON.stringify(Student),
+            dataType: "json"
+        });
+        return response;
+    }
+
+    this.AdminDashboard = function () {
+        var response = $http({
+            method: "post",
+            url: "Admin/Dashboard",
+            data: JSON.stringify(),
+            dataType: "json"
+        });
+        return response;
+    }
+
+    //Controller method call to load Nationality List
+    this.NationalityList = function () {
+        return $http.get("/Master/GetNationalityList")
+    }
+
+    //Controller method call to load Category List
+    this.CategoryList = function () {
+        return $http.get("/Master/GetCategoryList")
+    }
+
+    this.ReligiounList = function () {
+        return $http.get("/Master/GetReligiouList")
+    }
+
+});
+
